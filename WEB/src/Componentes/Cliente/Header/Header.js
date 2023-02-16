76d8d886dc64f700ae1bb1403f64126b5585255a
5lander <lachicaizaro@uide.edu.ec>
2023-02-16 02:31:21 -0500

    actualizacion 2:30

--- WEB/src/Componentes/Cliente/Header/Header.js
+++ WEB/src/Componentes/Cliente/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -7,11 +7,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export  function Header() {
-  return(
+export function Header() {
+  return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="/">UIDE</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -19,21 +19,14 @@ export  function Header() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <Nav.Link href="#action1">Admin site</Nav.Link>
+            <Nav.Link href="#action2">Ingresa</Nav.Link>
+            <NavDropdown title="Emprendimiento" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Emprendimientos</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
+                AgregarEmprendimientos
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -47,5 +40,5 @@ export  function Header() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
